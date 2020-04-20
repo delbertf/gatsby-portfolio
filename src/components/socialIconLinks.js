3877c2448584742d44d253c690dd553b27d2fae2
dfriesen <friesen.delbert@gmail.com>
2020-04-20 21:14:16 +0200

    update social links

--- src/components/socialIconLinks.js
+++ src/components/socialIconLinks.js
@@ -12,23 +12,14 @@ const SocialIconLinks = ({ iconClasses }) => {
 
   return (
     <>
-      <a href="https://github.com/cadekynaston" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/delbertf" target="_blank" rel="noopener noreferrer">
         <IconGithub classes={`${iconClasses}`} />
       </a>
-      <a href="https://www.linkedin.com/in/cadekynaston/" target="_blank" rel="noopener noreferrer">
+      <a href="www.linkedin.com/in/delbert-friesen/" target="_blank" rel="noopener noreferrer">
         <IconLinkedIn classes={`${iconClasses}`}  />
       </a>
-      <a href="https://twitter.com/cadekynaston" target="_blank" rel="noopener noreferrer">
+      <a href="https://twitter.com/friesen_delbert" target="_blank" rel="noopener noreferrer">
         <IconTwitter classes={`${iconClasses}`}  />
-      </a>
-      <a href="https://www.instagram.com/cadekynaston" target="_blank" rel="noopener noreferrer">
-        <IconInstagram classes={`${iconClasses}`}  />
-      </a>
-      <a href="https://codepen.io/cadekynaston/" target="_blank" rel="noopener noreferrer">
-        <IconCodepen classes={`${iconClasses}`}  />
-      </a>
-      <a href="https://codewars.com/users/ck1" target="_blank" rel="noopener noreferrer">
-        <IconCodewars classes={`${iconClasses}`}  />
       </a>
     </>
   )
